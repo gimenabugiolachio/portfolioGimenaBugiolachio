7f77a083e35d5ab0274ebe90127955fbbdd84e5e
Gimena <gimena.bugiolachio@gmail.com>
2022-08-13 19:28:46 -0300

    Update persona.service.ts

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -13,7 +13,7 @@ URL = 'http://localhost:8080/personas/';
   constructor(private http: HttpClient) { }
 
   public getPersona(): Observable<persona>{
-    
+    return this.http.get<persona>(this.URL + 'traer/perfil');
   }
 
 }
