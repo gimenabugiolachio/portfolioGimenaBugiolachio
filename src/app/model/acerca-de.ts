0452d20480e144896dcf09afd223b55ad87f3d55
Gimena <gimena.bugiolachio@gmail.com>
2022-08-21 03:17:56 -0300

    Edición terminada de componente Editar-acercaDe

--- src/app/model/acerca-de.ts
+++ src/app/model/acerca-de.ts
@@ -5,13 +5,15 @@ export class AcercaDe {
     edadA: string;
     mailA: string;
     telefonoA: string;
+    imgA: string;
 
-    constructor(id: number, nombreA: string, descripcionA: string, edadA: string, mailA: string, telefonoA: string){
+    constructor(id: number, nombreA: string, descripcionA: string, edadA: string, mailA: string, telefonoA: string, imgA: string){
         this.nombreA = nombreA;
         this.descripcionA = descripcionA;
         this.edadA = edadA;
         this.mailA = mailA;
         this.telefonoA = telefonoA;
         this.id = id;
+        this.imgA = imgA;
     }
 }
